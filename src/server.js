e0851b9e2b0f53718ccdaa2aed6eb2c0be2c9b32
Wagner Rangel <wrangeljr@gmail.com>
2025-08-02 15:59:07 -0300

    exemplo de requisicao post e get

--- src/server.js
+++ src/server.js
@@ -1,6 +1,27 @@
 import http from 'node:http'
 
+const users = []
 const server = http.createServer((req, res) => {
+
+    const {method, url} = req
+    console.log(req.headers)
+
+    if (method == 'GET' && url == '/users'){
+        return res
+        .setHeader('Content-type','application/json')
+        .end( JSON.stringify(users))    
+    }
+
+    if (method == 'POST' && url == '/users'){
+
+        users.push({
+            id: 1,
+            name:"Jonh Doe",
+            email:"jonhdoe@email.com",
+            role:"developer",
+        })
+        return res.end("Criacao de usario")
+    }
     return res.end("Hello Word")
 })
 
